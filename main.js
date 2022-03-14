dda96074a8e050515569132042f559a220269e4b
ThânQuân <quantv654@gmail.com>
2022-03-14 23:26:48 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+function goHome() {
+    window.location.href = "index.html"
+}
+
+function goMember() {
+    window.location.href = "member.html"
+}
+
+function goGallery() {
+    window.location.href = "gallery.html"
+}
